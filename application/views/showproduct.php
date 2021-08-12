4fda5ca26c19686c913f139d061fc7e73e48bb13
Nadanai Kurairat <614259015@webmail.npru.ac.th>
2021-08-12 23:22:10 +0700

    dd

--- application/views/showproduct.php
+++ application/views/showproduct.php
@@ -9,9 +9,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="<?php echo base_url('public'); ?>/index1.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
     <title>เลือกอาหาร</title>
 </head>
 <body>
+  <div class="bg">
 <br>
 
 <div class="container" align="left">
@@ -81,6 +87,7 @@
 </div>
 </div>
 <br>
+</div>
 </body>
 <?php $this->load->view('footer');  ?>
 </html>
